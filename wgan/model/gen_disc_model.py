79d6bae4e42ffb3d27be28152fa171c2c74fbc1e
Tanya <tanyach1997@gmail.com>
2020-10-24 23:58:49 +0530

    modified generator and discriminator for image size of 64

--- wgan/model/gen_disc_model.py
+++ wgan/model/gen_disc_model.py
@@ -3,62 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Critic(tf.keras.models.Model):
-    def __init__(self, clip_value):
-        super(Critic, self).__init__()
-        self.clip_value = clip_value
-        self.weight_constraint = tf.keras.constraints.MinMaxNorm(min_value=-self.clip_value, max_value=self.clip_value)
-        self.weight_init = tf.keras.initializers.RandomNormal()
-        self.conv_layer1 = tf.keras.layers.Conv2D(filters=64,
-                                                  kernel_size=(4, 4), strides=2, padding='same',
-                                                  kernel_initializer=self.weight_init,
-                                                  kernel_constraint=self.weight_constraint)
-        self.bn_layer1 = tf.keras.layers.BatchNormalization()
-
-        self.conv_layer2 = tf.keras.layers.Conv2D(filters=128,
-                                                  kernel_size=(4, 4), strides=2, padding='same',
-                                                  kernel_initializer=self.weight_init,
-                                                  kernel_constraint=self.weight_constraint)
-        self.bn_layer2 = tf.keras.layers.BatchNormalization()
-
-        self.conv_layer3 = tf.keras.layers.Conv2D(filters=256,
-                                                  kernel_size=(4, 4), strides=2, padding='same',
-                                                  kernel_initializer=self.weight_init,
-                                                  kernel_constraint=self.weight_constraint)
-        self.bn_layer3 = tf.keras.layers.BatchNormalization()
-
-        self.conv_layer4 = tf.keras.layers.Conv2D(filters=512,
-                                                  kernel_size=(4, 4), strides=2, padding='same',
-                                                  kernel_initializer=self.weight_init,
-                                                  kernel_constraint=self.weight_constraint)
-        self.bn_layer4 = tf.keras.layers.BatchNormalization()
-
-        self.conv_layer5 = tf.keras.layers.Conv2D(filters=1,
-                                                  kernel_size=(4, 4), kernel_initializer=self.weight_init,
-                                                  kernel_constraint=self.weight_constraint)
-        self.flatten_layer = tf.keras.layers.Flatten()
-        self.dense_layer = tf.keras.layers.Dense(1, activation='linear')
-        self.lrelu_layer = tf.keras.layers.LeakyReLU(0.2)
-
-    def call(self, input_tensor, training=None):
-        x = self.conv_layer1(input_tensor)
-        x = self.bn_layer1(x, training=training)
-        x = self.lrelu_layer(x)
-        x = self.conv_layer2(x)
-        x = self.bn_layer2(x, training=training)
-        x = self.lrelu_layer(x)
-        x = self.conv_layer3(x)
-        x = self.bn_layer3(x, training=training)
-        x = self.lrelu_layer(x)
-        x = self.conv_layer4(x)
-        x = self.bn_layer4(x, training=training)
-        x = self.lrelu_layer(x)
-        x = self.conv_layer5(x)
-        x = self.flatten_layer(x)
-        x = self.dense_layer(x)
-        return x
-
-
 class Generator(tf.keras.models.Model):
     def __init__(self):
         super(Generator, self).__init__()
@@ -98,4 +42,60 @@ class Generator(tf.keras.models.Model):
         x = self.relu_layer(x)
         x = self.conv_transpose_layer5(x)
         x = self.tanh_layer(x)
+        return x
+
+
+class Critic(tf.keras.models.Model):
+    def __init__(self, clip_value):
+        super(Critic, self).__init__()
+        self.clip_value = clip_value
+        self.weight_constraint = tf.keras.constraints.MinMaxNorm(min_value=-self.clip_value, max_value=self.clip_value)
+        self.weight_init = tf.keras.initializers.RandomNormal()
+        self.conv_layer1 = tf.keras.layers.Conv2D(filters=64,
+                                                  kernel_size=(4, 4), strides=2, padding='same',
+                                                  kernel_initializer=self.weight_init,
+                                                  kernel_constraint=self.weight_constraint)
+        self.bn_layer1 = tf.keras.layers.BatchNormalization()
+
+        self.conv_layer2 = tf.keras.layers.Conv2D(filters=128,
+                                                  kernel_size=(4, 4), strides=2, padding='same',
+                                                  kernel_initializer=self.weight_init,
+                                                  kernel_constraint=self.weight_constraint)
+        self.bn_layer2 = tf.keras.layers.BatchNormalization()
+
+        self.conv_layer3 = tf.keras.layers.Conv2D(filters=256,
+                                                  kernel_size=(4, 4), strides=2, padding='same',
+                                                  kernel_initializer=self.weight_init,
+                                                  kernel_constraint=self.weight_constraint)
+        self.bn_layer3 = tf.keras.layers.BatchNormalization()
+
+        self.conv_layer4 = tf.keras.layers.Conv2D(filters=512,
+                                                  kernel_size=(4, 4), strides=2, padding='same',
+                                                  kernel_initializer=self.weight_init,
+                                                  kernel_constraint=self.weight_constraint)
+        self.bn_layer4 = tf.keras.layers.BatchNormalization()
+
+        self.conv_layer5 = tf.keras.layers.Conv2D(filters=1,
+                                                  kernel_size=(4, 4), kernel_initializer=self.weight_init,
+                                                  kernel_constraint=self.weight_constraint)
+        self.flatten_layer = tf.keras.layers.Flatten()
+        self.dense_layer = tf.keras.layers.Dense(1, activation='linear')
+        self.lrelu_layer = tf.keras.layers.LeakyReLU(0.2)
+
+    def call(self, input_tensor):
+        x = self.conv_layer1(input_tensor)
+        x = self.bn_layer1(x)
+        x = self.lrelu_layer(x)
+        x = self.conv_layer2(x)
+        x = self.bn_layer2(x)
+        x = self.lrelu_layer(x)
+        x = self.conv_layer3(x)
+        x = self.bn_layer3(x)
+        x = self.lrelu_layer(x)
+        x = self.conv_layer4(x)
+        x = self.bn_layer4(x)
+        x = self.lrelu_layer(x)
+        x = self.conv_layer5(x)
+        x = self.flatten_layer(x)
+        x = self.dense_layer(x)
         return x
